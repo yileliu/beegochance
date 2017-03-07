450f79bf095c6d2618c25b2e2304e35d621b9198
yileliu <neal_liu@outlook.com>
2017-03-07 23:21:41 +0800

    replace static route

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "beegochance/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.SetStaticPath("/public", "static")
+	beego.DelStaticPath("/static")
 	beego.Run()
 }
-
